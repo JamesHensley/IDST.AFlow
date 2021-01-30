271700f0996c33f061224a00c59b6dd95e4a601f
James Hensley <JnsHens@hotmail.com>
2021-01-30 11:41:04 +0100

    Adding workflow models from old project... still need to fix problems

---
+++ IDST.AFlow.Workflow/Models/WorkflowEvolutionBlock.cs
@@ -0,0 +1,43 @@
+﻿using MicroFlow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowEngine.Workflow
+{
+    public enum WorkflowBlockStages
+    {
+        Navigate = 1,
+        Execute = 2,
+        Wait = 3,
+        Process = 4
+    }
+
+    /// <summary>
+    /// Represents an entire process (block) in the workflow
+    /// </summary>
+    public abstract class WorkflowEvolutionBlock : SyncActivity<List<KeyValuePair<string, string>>>
+    {
+        public string BlockName { get; set; }
+
+        /// <summary>
+        /// Collection of key/value pairs feeding this block
+        /// </summary>
+        public List<KeyValuePair<string, string>> InputData { get; set; }
+
+        /// <summary>
+        /// Collection of key/value pairs resulting from this block
+        /// </summary>
+        public List<KeyValuePair<string, string>> OutputData { get; set; }
+
+        public abstract Task<string> Navigate();
+        
+        public abstract Task<bool> Run();
+
+        public abstract Task<bool> Wait();
+
+        public abstract Task<List<KeyValuePair<string, string>>> Process();
+    }
+}
---
+++ IDST.AFlow.Workflow/Models/WorkflowEvolutionStage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowEngine.Workflow
+{
+    /// <summary>
+    /// Represents a specific stage in a workflow
+    /// </summary>
+    public abstract class WorkflowEvolutionStage
+    {
+        private List<KeyValuePair<string, string>> _ingestData;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string StageName { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public WorkflowEvolutionBlock Block { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="IngestData"></param>
+        public void Ingest(List<KeyValuePair<string, string>> IngestData) {
+            _ingestData = IngestData;
+        }
+
+        //Do something and return the new data
+        public abstract Task<WorkflowEvolutionStage> ProcessStage();
+    }
+}
